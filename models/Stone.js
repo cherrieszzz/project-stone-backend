f02d9e0244d76c2e05e47be9c674c68a7b30b1e7
imsesiki <greentacore@outlook.com>
2022-01-25 19:44:03 +0800

    new

---
+++ models/Stone.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const StoneSchema = new Schema({
+    name: String,
+    img_url:String,
+    info: String,
+    link: String,
+});
+const Stone = mongoose.model('Stone', StoneSchema)
+module.exports = Stone;
+//
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const fs = require('fs');
+const app = express();
+const router = express.Router();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connected to MongoDB
+const URI = 'mongodb+srv://zpyqaq:zpy020923@cluster0.6d1um.mongodb.net/stones-datebase?retryWrites=true&w=majority';
+mongoose.connect(URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}, err => {
+if(err) throw err;
+console.log('Connected to MongoDB!!!')
+});
+
+
+// Include controllers
+fs.readdirSync("controllers").forEach(function (file) {
+    if(file.substr(-3) == ".js") {
+    const route = require("./controllers/" + file)
+    route.controller(app)
+    }
+    })
+
+router.get('/', function(req, res) {
+    res.json({ message: 'API Initialized!'});
+});
+
+
+
+const port = process.env.API_PORT || 8081;
+app.use('/', router);
+
+app.listen(port, function() {
+    console.log(`api running on port ${port}`);
+});

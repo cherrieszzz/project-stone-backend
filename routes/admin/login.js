dbac725419f2da143984287b56fe1ee9253c7582
imessiki <greenteacore@outlook.com>
2022-02-08 23:01:03 +0800

    second commit

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -2,7 +2,12 @@ const express = require("express")
 var router = express.Router()
 
 router.get("/",(req,res) => {
-    res.send("login page")
+   
+})
+router.post("doLogin",(req,res)=>{
+    let username = req.body.username
+    let password = req.body.password
+    let verift = req.body.verify
 })
 
 module.exports = router
